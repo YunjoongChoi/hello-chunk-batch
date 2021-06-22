434f7b27a8e11c1de5e613e75fcfb036c0b1fccd
YunjoongChoi <83948750+YunjoongChoi@users.noreply.github.com>
2021-06-22 20:15:55 +0900

    Initial commit

---
+++ src/main/java/com/choimory/hellochunkbatch/HelloChunkBatchApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.hellochunkbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloChunkBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloChunkBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/hellochunkbatch/HelloChunkBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.hellochunkbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloChunkBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
